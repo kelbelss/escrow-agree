8a2e2196ad3feff1c94b7ae6fd8b78b77f33325e
kelbelss <124790103+kelbelss@users.noreply.github.com>
2025-01-06 15:15:10 +0200

    test setUp function

--- test/EscrowContractTest.t.sol
+++ test/EscrowContractTest.t.sol
@@ -4,12 +4,15 @@ pragma solidity 0.8.26;
 import {Test, console} from "lib/forge-std/src/Test.sol";
 
 contract EscrowContract is Test {
-// test events
-// event EscrowCreated(uint256 escrowId, address buyer, address seller, uint256 amount);
-// event FundsReleased(uint256 escrowId, address recipient);
-// event DisputeRaised(uint256 escrowId);
-// event DisputeResolved(uint256 escrowId, address recipient);
+    // test events
+    // test errors
+    // test each function with edge cases
 
-// test each function with edge cases
-// change requires to reverts with errors
+    // change requires to reverts with errors
+
+    address BUYER = makeAddr("buyer");
+    address SELLER = makeAddr("seller");
+    address ARBITRATOR = makeAddr("arbitrator");
+
+    function setUp() public {}
 }
